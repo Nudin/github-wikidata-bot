cb0e34dce4d066473a862254397bef7795b57ef5
Michael F. Schönitzer <michael@schoenitzer.de>
2019-04-21 17:26:27 +0200

    Make extractor the constructor of the class release

--- versionhandler.py
+++ versionhandler.py
@@ -14,21 +14,19 @@ def number_of_unique_values(values: List[str]) -> int:
 
 
 class Release:
-    VALID_TYPES = ["stable", "beta", "alpha", "rc", "unstable"]
+    __VALID_TYPES__ = ["stable", "beta", "alpha", "rc", "unstable"]
     type = None
     date = None
     __str__ = None
 
-    def __init__(self, string=None):
-        self.__str__ = string
+    def __init__(self, string, name=None, date=None, versiontype=None):
+        self.date = date
+        self.type = versiontype
+        self.__extract_version__(string, name)
 
     @property
     def string(self):
         return self.__str__
-
-    @string.setter
-    def string(self, string):
-        self.__str__ = string
 
     def __cmp__(self, other):
         if self.date is None or other.date is None:
@@ -62,79 +60,73 @@ class Release:
     def is_stable(self):
         return bool(self.type == "stable")
 
+    def __extract_version__(self, string: str, name: Optional[str] = None):
+        """
+        Heuristic to extract a version-number from a string.
 
-def extract_version(string: str, name: Optional[str] = None) -> Optional[Release]:
-    """
-    Heuristic to extract a version-number from a string.
+        See test file for supported formats. Returns None if no unambiguously
+        version number could be found.
 
-    See test file for supported formats. Returns None if no unambiguously
-    version number could be found.
+        :param string: the string to search
+        :param name: the name of the program
+        """
+        string = string.strip()
 
-    :param string: the string to search
-    :param name: the name of the program
-    :return: None or a tuple of two strings:
-             - type of version ("stable", "beta", "alpha", "rc" or "unstable")
-             - version number
-    """
-    string = string.strip()
-    version = Release()
+        # Remove a prefix of the name of the program if existent
+        if name:
+            namere = re.compile(r"^" + re.escape(name) + r"[ _/-]?", re.IGNORECASE)
+            string = re.sub(namere, "", string)
 
-    # Remove a prefix of the name of the program if existent
-    if name:
-        namere = re.compile(r"^" + re.escape(name) + r"[ _/-]?", re.IGNORECASE)
-        string = re.sub(namere, "", string)
+        # Remove common prefixes/postfixes
+        string = re.sub(
+            r"^(releases|release|rel|version|vers|v\.)[ _/-]?",
+            "",
+            string,
+            flags=re.IGNORECASE,
+        )
+        string = re.sub(r"^(v|r)(?<![0-9])", "", string, flags=re.IGNORECASE)
+        string = re.sub(
+            r"(^|[._ -])(final|release)([._ -]|$)", " ", string, flags=re.IGNORECASE
+        )
 
-    # Remove common prefixes/postfixes
-    string = re.sub(
-        r"^(releases|release|rel|version|vers|v\.)[ _/-]?",
-        "",
-        string,
-        flags=re.IGNORECASE,
-    )
-    string = re.sub(r"^(v|r)(?<![0-9])", "", string, flags=re.IGNORECASE)
-    string = re.sub(
-        r"(^|[._ -])(final|release)([._ -]|$)", " ", string, flags=re.IGNORECASE
-    )
+        # Replace underscore/hyphen with dots if only underscores/hyphens are used
+        if re.fullmatch(r"[0-9_]*", string):
+            string = string.replace("_", ".")
+        if re.fullmatch(r"[0-9-]*", string):
+            string = string.replace("-", ".")
 
-    # Replace underscore/hyphen with dots if only underscores/hyphens are used
-    if re.fullmatch(r"[0-9_]*", string):
-        string = string.replace("_", ".")
-    if re.fullmatch(r"[0-9-]*", string):
-        string = string.replace("-", ".")
+        # Detect type of version
+        words = ["stable", "beta", "alpha", "rc", "pre", "preview", "b\d", "dev"]
+        res = re.findall(r"(" + "|".join(words) + r")", string, re.IGNORECASE)
+        if number_of_unique_values(res) == 1:
+            self.type = res[0].lower()
+            if self.type[0] == "b":
+                self.type = "beta"
+            if self.type not in self.__VALID_TYPES__:
+                self.type = "unstable"
+        elif number_of_unique_values(res) > 1:
+            raise Exception("Multiple possible interpretations found")
 
-    # Detect type of version
-    words = ["stable", "beta", "alpha", "rc", "pre", "preview", "b\d", "dev"]
-    res = re.findall(r"(" + "|".join(words) + r")", string, re.IGNORECASE)
-    if number_of_unique_values(res) == 1:
-        version.type = res[0].lower()
-        if version.type[0] == "b":
-            version.type = "beta"
-        if version.type not in version.VALID_TYPES:
-            version.type = "unstable"
-    elif number_of_unique_values(res) > 1:
-        return None
+        # Detect version string
+        gen = re.compile(
+            r"((?<=\s)|^)(\d{1,3}(\.\d{1,3})+[a-z]?([._ -]?(alpha|beta|pre|rc|b|stable|preview|dev)[._-]?\d*|-\d+)?)(\s|$)",
+            re.IGNORECASE,
+        )
+        res = gen.findall(string)
+        # remove "stable" from version string
+        res = list(map(lambda s: re.sub(r"[._-]stable[._-]?", "", s[1]), res))
+        if number_of_unique_values(res) == 1:
+            self.__str__ = res[0]
+        else:
+            # If the string contains nothing but a version-number we are more gratefully with what we accept
+            full = re.compile(r"[1-9]\d{0,4}", re.IGNORECASE)
+            if full.fullmatch(string):
+                self.__str__ = string
 
-    # Detect version string
-    gen = re.compile(
-        r"((?<=\s)|^)(\d{1,3}(\.\d{1,3})+[a-z]?([._ -]?(alpha|beta|pre|rc|b|stable|preview|dev)[._-]?\d*|-\d+)?)(\s|$)",
-        re.IGNORECASE,
-    )
-    res = gen.findall(string)
-    # remove "stable" from version string
-    res = list(map(lambda s: re.sub(r"[._-]stable[._-]?", "", s[1]), res))
-    if number_of_unique_values(res) == 1:
-        version.string = res[0]
-    else:
-        # If the string contains nothing but a version-number we are more gratefully with what we accept
-        full = re.compile(r"[1-9]\d{0,4}", re.IGNORECASE)
-        if full.fullmatch(string):
-            version.string = string
-
-    if version.string is not None:
         # if we don't find any indication about the state of the version,
         # we assume it's a stable version
-        if version.type is None:
-            version.type = "stable"
-        return version
+        if self.type is None:
+            self.type = "stable"
 
-    return None
+        if self.__str__ is None:
+            raise Exception("No version number found")
